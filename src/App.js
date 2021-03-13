9afcbc076c2b9191b7ac545367660d979e7e6641
coder-derie <77130913+coder-derie@users.noreply.github.com>
2021-03-13 10:50:59 -0500

    Updated footer links

--- src/App.js
+++ src/App.js
@@ -9,14 +9,22 @@ export default function App(){
       <Weather />
       </div>
       <footer className="footer">
+        This project was created by{" "}
+        <a href="https://www.linkedin.com/in/deiriaimyers/" target="_blank" rel="noreferrer">
+          Deiriai Myers
+          </a>{" "} 
+         and is open-sourced on 
+        {" "} 
         <a
           href="https://github.com/coder-derie/react-project"
           target="_blank" rel="noreferrer"
         >
-          Open-source code
-        </a>{" "}
-        by {" "}
-        <a href="https://www.linkedin.com/in/deiriaimyers/" target="_blank" rel="noreferrer">Deiriai Myers</a> .
+         Github
+        </a> {" "}
+        and hosted on {" "}
+        <a href="https://elegant-volhard-77b09f.netlify.app/" target="_blank" rel="noreferrer">
+          Netlify
+        </a>
       </footer>
     </div>
   );
